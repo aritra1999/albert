5c04e2f28f235ea08e08cfcc3113b0496c39d4d6
Aritra Mondal <aritra041999.dev@gmail.com>
2023-04-08 11:03:24 +0530

    Added Debug component for Events

---
+++ src/lib/components/Debug/Debug.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let date: Date; 
+</script>   
+<div class="p-4 text-sm text-gray-800 rounded-lg bg-slate-100" role="alert">
+    <div class="font-bold text-xl"> Debug </div>
+    <div> Date String: {date.toDateString()} </div>
+    <div> Date Locale String: {date.toLocaleDateString()}</div>
+    <div> Date: {date.getDate()}</div>
+    <div> Month: {date.getMonth()}</div>
+    <div> Day: {date.getDay()}</div>
+    <div> Year: {date.getFullYear()}</div>
+    <div> Time: {date.getTime()}</div>
+</div>
