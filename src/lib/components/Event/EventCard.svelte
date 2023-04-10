68f077b5043cd881817dbc63e905946f3d85b46f
Aritra Mondal <aritra041999.dev@gmail.com>
2023-04-10 10:54:41 +0530

    Added show more and improved "Done" UX

--- src/lib/components/Event/EventCard.svelte
+++ src/lib/components/Event/EventCard.svelte
@@ -6,11 +6,14 @@
     export let event: EventInterface;
 
     let done = false; 
+    let showMore = event.description ? event.description.length > 25 ? false : true : false;
+    
     const toggleDone = () => done = !done; 
-
+    const toggleShowMore = () => showMore = !showMore;
+    
 </script>
 
-<div class="bg-slate-100 my-2 p-2 rounded">
+<div class="{done ? 'bg-stone-100': 'bg-slate-100'} my-2 p-2 rounded">
     <div class="flex divide-x-4 divide-blue-600">
         <div class="w-36 flex flex-col justify-center py-2 px-2">
             {#if event.timestamp.time}
@@ -18,27 +21,38 @@
                 <div class="text-xs text-slate-500">{getTimeDifference(event.timestamp.time)}</div>
             {/if}
         </div>
-        <div class="w-full flex flex-col justify-center py-2 px-4">
-            <div class="flex justify-between">
+        <div class="w-full flex flex-col justify-center py-2 px-4  {done ? 'line-through': ''}">
+            <div class="flex justify-between relative">
                 <div>
-                    <div class="text-lg font-semibold mb-2 {done ? 'line-through': ''}">{event.title}</div>
-                    {#if event.timestamp.time && event.timestamp.time[1]}
-                        <div class="text-xs text-slate-500">
-                            {formatTime(event.timestamp.time[0])}{#if event.timestamp.time[1]} - {formatTime(event.timestamp.time[1])}{/if}
-                        </div>
+                    <div class="text-lg font-semibold">{event.title}</div>
+                </div>
+                <div class="absolute -top-2 -right-4">
+                    {#if !done }
+                        <button on:click={toggleDone} class="text-slate-700 hover:bg-blue-500 hover:text-white text-xs px-2 py-0.5 rounded-full">Mark Done?</button>
+                    {:else}
+                        <button on:click={toggleDone}>✅</button>
                     {/if}
                 </div>
-                {#if !done }
-                    <button on:click={toggleDone} class="text-slate-700 hover:bg-blue-500 hover:text-white text-xs px-2 py-0.5 rounded-full">Mark Done?</button>
-                {:else}
-                    <button on:click={toggleDone} class="bg-green-100  text-green-500 hover:bg-green-300 hover:text-white text-xs px-2 py-0.5 rounded-full">Done</button>
+            </div>
+            <div>
+                {#if event.timestamp.time && event.timestamp.time[1]}
+                    <div class="text-xs text-slate-500">
+                        {formatTime(event.timestamp.time[0])}{#if event.timestamp.time[1]} - {formatTime(event.timestamp.time[1])}{/if}
+                    </div>
                 {/if}
             </div>
-            {#if event.description}
-                <div class="prose prose-sm text-sm">
-                    <SvelteMarkdown source={event.description} />
-                </div>
-            {/if}
+            <div class="w-full relative">
+                {#if event.description}
+                    <div class="prose prose-sm text-sm mb-3">
+                        <SvelteMarkdown source={event.description.slice(0, showMore ? event.description.length : 25)} />
+                    </div>
+                    {#if event.description.length > 25}
+                        <button on:click={toggleShowMore} class="bg-blue-100 px-2 py-0.5 text-blue-500 rounded-full text-xs absolute -bottom-2 -right-4">
+                            {#if showMore} less {:else} more {/if}
+                        </button>    
+                    {/if}
+                {/if}
+            </div>
         </div>
     </div>
 </div>
